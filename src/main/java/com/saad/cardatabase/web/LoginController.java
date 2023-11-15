259b8ea5006154fdd9cfa9a3ba2b9f42141d1ec1
Saad <saadnajm.msc@gmail.com>
2023-11-15 09:39:54 +0100

    login controller

--- src/main/java/com/saad/cardatabase/web/LoginController.java
+++ src/main/java/com/saad/cardatabase/web/LoginController.java
@@ -1,7 +1,14 @@
 package com.saad.cardatabase.web;
 
+import com.saad.cardatabase.domain.AccountCredentials;
 import com.saad.cardatabase.service.JwtService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +20,16 @@ public class LoginController {
     public LoginController(JwtService jwtService, AuthenticationManager authenticationManager) {
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials) {
+        UsernamePasswordAuthenticationToken creds = new UsernamePasswordAuthenticationToken(credentials.username(),
+                credentials.password());
+        Authentication auth = authenticationManager.authenticate(creds);
+        // Generate token
+        String jwts = jwtService.getToken(auth.getName());
+        // Build response with the generated token
+        return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, "Bearer" + jwts)
+                .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization").build();
     }
 }
